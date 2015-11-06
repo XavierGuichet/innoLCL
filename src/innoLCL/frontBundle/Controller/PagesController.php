7e071d79e3c444de826761c4ad0890ad7eff6b12
Derjuju <derjuju@gmail.com>
2015-11-06 01:21:12 +0100

    Pages Mentions et Règlement

---
+++ src/innoLCL/frontBundle/Controller/PagesController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace innoLCL\frontBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class PagesController extends Controller
+{
+    public function mentionsAction(Request $request) // /mentions-legales
+    {
+        return $this->render('innoLCLfrontBundle:Pages:mentions.html.twig');
+    }
+    
+    public function reglementAction(Request $request) // /reglement
+    {
+        return $this->render('innoLCLfrontBundle:Pages:reglement.html.twig');
+    }
+}
